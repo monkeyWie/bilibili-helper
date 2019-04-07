14ece2634b517491f8de360b089f1923238fc94c
tianpian <tianpian@aliyun.com>
2019-04-07 23:33:56 +0800

    1. 修复B站视频无法获取下载链接的bug; 2. 解决Flash播放模式无法注入代码获取下载链接的问题;

--- src/hook.js
+++ src/hook.js
@@ -1,29 +1,14 @@
-import 'core-js/es6/math'
 import api from './common/api'
-import { jQuery as $ } from 'proxyee-down-extension-sdk'
 
 global.onResolve = async function(request) {
-  let html
-  $.ajax({
-    url: request.url,
-    async: false,
-    contentType: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    headers: {
-      Host: 'www.bilibili.com',
-      Referer: 'https://www.bilibili.com/'
-    },
-    success(result) {
-      html = result
-    }
-  })
-  const cidMatch = /"cid":(\d+),/.exec(html)
-  if (cidMatch) {
-    const cid = cidMatch[1]
-    const title = /<h1\s+title="([^<>]*)"\s*>/.exec(html)[1]
+  const aid = request.url.replace(/^.*\/av(\d+).*$/)
+  const viewInfo = await api.getViewInfo(aid)
+  if (viewInfo) {
+    const cid = viewInfo.data.cid
+    const title = viewInfo.data.title
     const quality = pdown.settings.quality || 80
-    const seasonType = request.url.indexOf('/bangumi/') != -1 ? 1 : 0
     try {
-      const resolveForm = await api.buildResolveForm(cid, quality, seasonType, title)
+      const resolveForm = await api.buildResolveForm(aid, cid, quality, title)
       resolveForm.data = { cid: cid, quality }
       resolveForm.config = { connections: 16 }
       return resolveForm
--- src/web.js
+++ src/web.js
@@ -1,28 +1,47 @@
 window.onload = () => {
   const checkLoad = setInterval(() => {
-    //检查播放器是否加载完成
-    if (window.$ && $('div.bilibili-player-video-control>div').size() > 0) {
+    // 检查播放器是否加载完成
+    if (window.$ && ($('div.bilibili-player-video-control>div').length > 0) || $('object.player').length > 0) {
       clearInterval(checkLoad)
-      const api = require('./common/api').default
+      const api = require('./common/api')
       const pdownSdk = require('proxyee-down-extension-sdk').default
-      const downDivJq =
+      let downDivJq
+      if ($('object.player').length > 0) {
+        const downDivStyle = require('./views/DownButtonFlash').default.Style
+        $('html>head').append(`<style>${downDivStyle}</style>`)
+        downDivJq = require('./views/DownButtonFlash').default.Button
+        const waitInjectButton = setInterval(() => {
+          if ($('object.player') && $('object').context.readyState === 'complete') {
+            clearInterval(waitInjectButton)
+            $('#arc_toolbar_report>.ops').append(downDivJq)
+          }
+        }, 200)
+      } else {
+        downDivJq =
         $('div.old-btn').size() > 0
           ? require('./views/DownButtonNew').default
           : require('./views/DownButtonOld').default
-      $('div.bilibili-player-video-control div.bilibili-player-video-btn-start').after(downDivJq)
+        $('div.bilibili-player-video-control div.bilibili-player-video-btn-start').after(downDivJq)
+      }
+      
       downDivJq.on('click', 'li[data-value]', async function() {
         const quality = $(this).attr('data-value')
         try {
-          let seasonType
+          const aid = window.__INITIAL_STATE__.aid || window.location.href.replace(/^.*\/av(\d+).*$/, '$1')
           let cid
+          let title
           if (window.__INITIAL_STATE__.videoData) {
-            seasonType = 0
             cid = window.__INITIAL_STATE__.videoData.cid
-          } else {
-            seasonType = 1
+            title = window.__INITIAL_STATE__.videoData.title
+          } else if (window.__INITIAL_STATE__.epInfo) {
             cid = window.__INITIAL_STATE__.epInfo.cid
+            title = window.__INITIAL_STATE__.epInfo.title
+          } else {
+            const viewInfo = await api.getViewInfo(aid)
+            cid = viewInfo.data.cid
+            title = viewInfo.data.title
           }
-          const createForm = await api.buildResolveForm(cid, quality, seasonType, $('h1[title]').attr('title'))
+          const createForm = await api.buildResolveForm(aid, cid, quality, title)
           pdownSdk.createTask(createForm)
         } catch (e) {
           alert('获取下载链接失败')
